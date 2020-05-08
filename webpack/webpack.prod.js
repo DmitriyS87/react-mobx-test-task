bfdfc1dd125d220eba07ea9bef99b6bd4d12ea2c
DmitriyS87 <schukindmitriy@gmail.com>
2020-05-08 20:30:09 +0800

    changed webpack config

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  output: {
+    publicPath: '.'
+  }
 });
